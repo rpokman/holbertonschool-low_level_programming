f3c71c9fef96634751d3024519b425f86bc70c34
rpokman <roymathis74200@gmail.com>
2025-06-16 12:39:16 +0000

    unit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-	put("Programming is like building a multilingual puzzle);
-	return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
