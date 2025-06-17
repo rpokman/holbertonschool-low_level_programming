af8ff2946a2ff330c122e14f9c4a6c9109282d12
root <roymathis74200@gmail.com>
2025-06-17 09:41:12 +0000

    unit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,33 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include <stdio.h> /* Ajout de la bibliothèque pour printf */
 
 /**
- * main - Entry point
- *
+ * main - Affiche le dernier chiffre d'un nombre aléatoire et son état
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int last_digit;
+int n;
+int lastd; /* Déclaration de la variable lastd */
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastd = n % 10;
 
-	if (last_digit > 5)
-	{
-		printf("The last digit of %d is %d and is greater than 5\n", n, last_digit);
-	}
-	else if (last_digit == 0)
-	{
-		printf("The last digit of %d is %d and is 0\n", n, last_digit);
-	}
-	else
-	{
-		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-	}
-
-	return (0);
+if (lastd > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+}
+else if (lastd == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastd);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+}
+return (0);
 }
