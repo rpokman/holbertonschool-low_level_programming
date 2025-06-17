4a50615a90b4c3d4765f366e3d59105aa55361b6
root <roymathis74200@gmail.com>
2025-06-17 12:37:27 +0000

    unit

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet <= 'a')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+}
