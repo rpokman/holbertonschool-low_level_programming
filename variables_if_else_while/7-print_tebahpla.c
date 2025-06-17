55e53f8d71311e553f9af83603507512b76f52eb
root <roymathis74200@gmail.com>
2025-06-17 12:47:25 +0000

    unit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-	char alphabet = 'z';
+	char alphabet = 'a';
 
-	while (alphabet <= 'a')
+	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
-		alphabet++;
+		alphabet--;
 	}
 
 	putchar('\n');
