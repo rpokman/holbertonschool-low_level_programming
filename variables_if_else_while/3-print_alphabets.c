5f3794febbe6f86dcf0efd5c615e3f297cd45fb4
root <roymathis74200@gmail.com>
2025-06-17 11:27:33 +0000

    unit

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -12,17 +11,17 @@ int main(void)
 	char lower = 'a';
 	char upper = 'A';
 
-while (lower <= 'z')
+	while (lower <= 'z')
 	{
 		putchar(lower);
 		lower++;
 	}
 
-while (upper <= 'Z')
+	while (upper <= 'Z')
 	{
 		putchar(upper);
-                upper++;
-        }
+		upper++;
+	}
 
 	putchar('\n');
 	return (0);
