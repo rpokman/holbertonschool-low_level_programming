32c15ef6d66e81c94aad547fef3ed026c1458673
root <roymathis74200@gmail.com>
2025-06-18 08:30:34 +0000

    unit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
