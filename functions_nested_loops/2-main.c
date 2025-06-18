6627fe669fd607612ce7227c96f019974269c7e4
root <roymathis74200@gmail.com>
2025-06-18 10:46:59 +0000

    unit

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/* Prototype de la fonction print_alphabet */
+void print_alphabet_x10(void);
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 en cas de succès
+ */
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase x10, followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
