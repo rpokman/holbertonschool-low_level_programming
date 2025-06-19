4ad3100245a0166f29812eb731d60c083e345006
root <roymathis74200@gmail.com>
2025-06-19 17:49:38 +0000

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  *print_last_digit - Prints the last digit of a number
- * @n: The number to be treated
+ *@i: variable a traité
  * Return: value of the last digit of number
  */
 int print_last_digit(int i)
