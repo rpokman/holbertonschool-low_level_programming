a2ab137aa90e34a6f841966cb46fde698cd787c4
root <roymathis74200@gmail.com>
2025-06-20 16:08:45 +0000

    a

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@
 void jack_bauer(void)
 {
 	int heure, minute;
-	for (heure = 0; heure > 24; heure ++)
+	for (heure = 0; heure < 24; heure ++)
 	{
-		for (minute = 0; minute > 60; minute ++)
+		for (minute = 0; minute < 60; minute ++)
 		{
-			printf("%02d:%02d0\n", heure, minute);
+			printf("%02d:%02d\n", heure, minute);
 		}
 	}
 }
