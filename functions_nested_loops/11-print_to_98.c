542d3542026884425ef89b98bbfbb6632217dcf8
root <roymathis74200@gmail.com>
2025-06-20 17:25:27 +0000

    a

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - display all to 98
+ * @n: strating point
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
