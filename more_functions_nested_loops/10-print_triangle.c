4baa93e460696072149b979b0d7f12af74a0dc94
root <roymathis74200@gmail.com>
2025-06-23 16:02:08 +0000

    malik

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,8 +10,12 @@ void print_triangle(int size)
 	int a;
 	int b;
 
-	if (size > 0)
+	if (size <= 0)
 		{
+		_putchar('\n');
+		return;
+		}
+
 		for (a = 1; a <= size; a++)
 		{
 			for (b = 1; b <= (size - a); b++)
@@ -24,5 +28,4 @@ void print_triangle(int size)
 			}
 			_putchar('\n');
 		}
-	}
 }
