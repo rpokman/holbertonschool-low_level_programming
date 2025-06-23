46ab3b2063c609cd7bf2071d4ebc5f8df507075d
root <roymathis74200@gmail.com>
2025-06-23 13:25:38 +0000

    unit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,16 @@ int main(void)
 	{
 		if ((number % 3) == 0 && (number % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 
 		else if ((number % 3) == 0) 
 				{
-					printf("Fizz ");
+					printf("Fizz");
 				}
 					else if ((number % 5) == 0)
 					{
-						printf("Buzz ");
+						printf("Buzz");
 					}
 					else
 					{
