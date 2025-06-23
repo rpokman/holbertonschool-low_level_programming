297c5cf69949ea928247659090243e99f4642c62
root <roymathis74200@gmail.com>
2025-06-23 11:10:55 +0000

    unit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+			_putchar('\n');
+}
