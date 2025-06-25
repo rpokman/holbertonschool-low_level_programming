617ba8fb32398d14cc7377199c6791ce07a845ac
root <roymathis74200@gmail.com>
2025-06-25 12:51:18 +0000

    unit

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _atoi - function
+ *
+ *@str: string
+ */
+int _atoi(char *s)
+{
+	int a = 0;
+	int sign = 1;
+	int result = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+
+	}
+	return result * sign;
+}
