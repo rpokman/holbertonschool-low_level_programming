e1373c5f29a9aa2f6e8e3485e3a43064f145dd95
root <roymathis74200@gmail.com>
2025-06-26 08:21:44 +0000

    unit

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,18 +3,29 @@
 /**
  * _atoi - function
  *
- *@str: string
+ *@s: string
+ *
+ * Return: The converted integer value
  */
 int _atoi(char *s)
 {
 	int a = 0;
 	int sign = 1;
 	int result = 0;
+	int started = 0;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		if (s[a] >= '0' && s[a] <= '9')
+		if (s[a] == '-')
+			sign *= -1;
+		else if (s[a] >= '0' && s[a] <= '9')
+		{
+			result = result * 10 + (s[a] - '0');
+			started = 1;
+		}
+		else if (started)
+			break;
 
 	}
-	return result * sign;
+	return (result * sign);
 }
