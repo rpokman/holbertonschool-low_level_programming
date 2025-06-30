35f3f7d79a9e636945fbdb1fbe23cb884c886f91
rpokman <roymathis74200@gmail.com>
2025-06-30 15:12:17 +0200

    Update main function to test _strstr implementation for substring search

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,26 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * _strstr - Locates a substring
+ * @haystack: The string to search in
+ * @needle: The substring to search for
+ *
+ * Return: Pointer to the beginning of located substring,
+ * or NULL if substring is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
-    for ()
+    unsigned a, b;
+    for (a = 0; haystack[a] != '\0'; a++)
+    {
+        for (b = 0; needle[b] != '\0'; b++)
+        {
+            if (haystack[a + b] != needle[b])
+            break;
+        }
+        if (needle[b] == '\0')
+            return (&haystack[a]);
+    }
+    return (NULL);
 }
