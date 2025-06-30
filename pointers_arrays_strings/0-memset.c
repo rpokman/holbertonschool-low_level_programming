eab95555b1a64c4325837524013081d97c457ea7
rpokman <roymathis74200@gmail.com>
2025-06-30 12:53:40 +0200

    Improve documentation for _memset function in 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
 /**
- *_memset - function
+ * _memset - Fills the first n bytes of the memory area pointed to by s
+ *           with the constant byte b.
+ * @s: Pointer to the memory area to fill.
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
