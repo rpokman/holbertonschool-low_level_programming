4c919f14fed327837209996f5a0e49f3af80db83
rpokman <roymathis74200@gmail.com>
2025-07-01 10:44:31 +0200

    Fix formatting and indentation in _memset function

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area pointed to by s
- *           with the constant byte b.
- * @s: Pointer to the memory area to fill.
- * @b: The constant byte to fill the memory with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the memory area s.
- */
+* _memset - Fills the first n bytes of the memory area pointed to by s
+*           with the constant byte b.
+* @s: Pointer to the memory area to fill.
+* @b: The constant byte to fill the memory with.
+* @n: The number of bytes to be filled.
+*
+* Return: A pointer to the memory area s.
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int a;
+		unsigned int a;
 
-        for (a = 0; a < n; a++)
-        {
-                s[a] = b;
-        }
-        return (s);
+		for (a = 0; a < n; a++)
+		{
+			s[a] = b;
+		}
+		return (s);
 }
