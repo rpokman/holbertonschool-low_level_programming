b0684bb580750a20acb03e8aae79b29eb30f2ee0
rpokman <roymathis74200@gmail.com>
2025-07-01 10:39:21 +0200

    Fix formatting and indentation in _puts_recursion and _strlen_recursion functions

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string recursively
- * @s: Pointer to the string
- */
+* _strlen_recursion - Returns the length of a string recursively
+* @s: Pointer to the string
+* Return: The length of the string (as an int)
+*/
 int _strlen_recursion(char *s)
 {
-    if (s[0] == '\0')
-    {
-        return (0);
-    }
-    return (1 + _strlen_recursion(s + 1));
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
 }
