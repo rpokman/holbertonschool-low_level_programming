c628cefb8b37cb876320ced33b4d00971bf09880
rpokman <roymathis74200@gmail.com>
2025-07-01 09:57:11 +0200

    Implement _puts_recursion function and update main.h with function prototypes

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line, recursively
+ * @s: Pointer to the string to be printed
+ *
+ * Description: This function prints one character at a time
+ * using recursion until it reaches the null terminator ('\0'),
+ * then prints a newline character.
+ */
+void _puts_recursion(char *s)
+{
+    if (s[0] == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(s[0]);
+    _puts_recursion(s + 1);
+}
