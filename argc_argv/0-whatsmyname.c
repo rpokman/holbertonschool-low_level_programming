ad75ff505ab27f1c51495bfce3a16f37c344f3d9
rpokman <roymathis74200@gmail.com>
2025-07-02 21:44:33 +0200

    Add 0-whatsmyname.c to print the program's name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point of the program
+* @argc: number of arguments passed to the program (unused)
+* @argv: array of strings containing the arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
