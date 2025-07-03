c61e8a4a15149fc9ba5250e87db3ab80b02b053e
rpokman <roymathis74200@gmail.com>
2025-07-03 10:08:20 +0200

    Fix formatting in 1-args.c and add 2-args.c with initial implementation

---
+++ argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point of the program
+* @argc: number of arguments passed to the program (unused)
+* @argv: array of strings containing the arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
