a1f419043c2876b02da848a2d6b85b09c03b2b7e
rpokman <roymathis74200@gmail.com>
2025-07-03 11:07:05 +0200

    Update 2-args.c to remove unused annotation for argc and ensure proper formatting

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -3,10 +3,18 @@
 
 /**
 * main - Entry point of the program
-* @argc: number of arguments passed to the program (unused)
+* @argc: number of arguments passed to the program
 * @argv: array of strings containing the arguments
 *
 * Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
+	int a = 0;
+    
+    for (a = 0; a <argc; a++)
+    {
+    printf("%s\n", argv[a]);
+    }
+	return (0);
+}
