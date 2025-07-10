e5faaeb5a8b8eb137915a845be54c32aad8d8f3c
rpokman <roymathis74200@gmail.com>
2025-07-10 10:14:30 +0200

    Refactor print_dog function for improved readability and formatting

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,13 +2,20 @@
 #include "dog.h"
 #include "main.h"
 
+/**
+ * print_dog - Prints a struct dog
+ * @d: Pointer to struct dog
+ *
+ * If an element is NULL, it prints (nil) instead.
+ * If d is NULL, it does nothing.
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-    {
-        printf ("(nil)\n");
-        return;
-    }
+	{
+		printf("(nil)\n");
+		return;
+	}
 
 		printf("name: %s\n", d->name);
 		printf("age: %f\n", d->age);
