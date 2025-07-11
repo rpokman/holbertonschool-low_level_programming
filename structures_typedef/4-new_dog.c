cf6c508e4477da1d2bd059c930779d037eb62aa7
rpokman <roymathis74200@gmail.com>
2025-07-11 10:06:43 +0200

    Refactor new_dog function and add helper functions for string manipulation

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to measure
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -12,33 +47,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new;
-	int len_name, len_owner;
+	dog_t *d;
+	int len1, len2;
 
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
+	if (!name || !owner)
 		return (NULL);
 
-	len_name = strlen(name);
-	new->name = malloc(sizeof(char) * (len_name + 1));
-	if (new->name == NULL)
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (!d->name)
 	{
-		free(new);
+		free(d);
 		return (NULL);
 	}
 
-	len_owner = strlen(owner);
-	new->owner = malloc(sizeof(char) * (len_owner + 1));
-	if (new->owner == NULL)
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (!d->owner)
 	{
-		free(new->name);
-		free(new);
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
 
-	strcpy(new->name, name);
-	strcpy(new->owner, owner);
-	new->age = age;
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+	d->age = age;
 
-	return (new);
+	return (d);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,26 +2,23 @@
 #define DOG_H
 
 /**
- * struct dog - Structure to represent a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * struct dog - defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
 	char *name;
-
 	float age;
-
 	char *owner;
-
 };
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif
