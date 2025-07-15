00576312fb783e24684cd7f43ed6774bc4ac5814
rpokman <roymathis74200@gmail.com>
2025-07-15 09:46:48 +0200

    Add print_elem and print_elem_hex functions with main for testing

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - Calls a function to print a name
+ * @name: The name to print
+ * @f: Pointer to a function that takes a char * and returns void
+ *
+ * Description: If both the function pointer and name are not NULL,
+ * this function calls the function pointed to by @f with @name as argument.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
