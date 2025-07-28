65bd5a525a98bf676bcd8db21e493e0505b67631
rpokman <roymathis74200@gmail.com>
2025-07-28 13:32:09 +0200

    style: improve formatting and consistency in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,30 +1,30 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list
- * @head: double pointer to the head of the list
- * @str: string to duplicate and store in the new node
- *
- * Return: the address of the new element, or NULL if it failed
- */
+* add_node - Adds a new node at the beginning of a list_t list
+* @head: double pointer to the head of the list
+* @str: string to duplicate and store in the new node
+*
+* Return: the address of the new element, or NULL if it failed
+*/
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
+	list_t *new;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-    new->len = strlen(str);
-    new->next = *head;
-    *head = new;
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
