46b8271e0e9db138842b3e0e46933dd68e503784
rpokman <roymathis74200@gmail.com>
2025-07-28 13:51:55 +0200

    style: improve documentation formatting in add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
-* add_node - Adds a new node at the beginning of a list_t list
-* @head: double pointer to the head of the list
-* @str: string to duplicate and store in the new node
-*
-* Return: the address of the new element, or NULL if it failed
-*/
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to duplicate and store in the new node
+ *
+ * Return: The address of the new element, or NULL if it failed
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
