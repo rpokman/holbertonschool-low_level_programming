597d7172006f57d7f97798015d9c8ab8dc486e99
rpokman <roymathis74200@gmail.com>
2025-07-28 11:49:12 +0200

    style: format struct list_s for improved readability

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,26 +5,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdarg.h>
 #include <unistd.h>
 
 /**
- * struct list_s
- * @str: string
+ * struct list_s - singly linked list node structure
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: Structure representing a node in a singly linked list
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+
+	unsigned int len;
+
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+
 list_t *add_node_end(list_t **head, const char *str);
+
 void free_list(list_t *head);
 
 #endif
