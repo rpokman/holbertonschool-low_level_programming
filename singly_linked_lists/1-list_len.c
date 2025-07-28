32acd5a9e6241ee0591bc0872453bc1b194d430b
rpokman <roymathis74200@gmail.com>
2025-07-28 13:01:03 +0200

    feat: implement list_len function to count elements in list_t

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Retourne le nombre d’éléments dans une liste list_t
+ * @h: pointeur vers la tête de la liste
+ *
+ * Return: le nombre de nœuds
+ */
+size_t list_len(const list_t *h)
+{
+
+size_t count = 0;
+
+for (; h != NULL; h = h->next)
+{
+    count++;
+}
+return (count);
+}
