f3f3e211e044cb77438acbfcc438015e678d9cda
rpokman <roymathis74200@gmail.com>
2025-07-29 09:33:07 +0200

    feat: add main function to test print_dlistint and demonstrate doubly linked list functionality

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,26 +1,19 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a list_t list
- * @h: pointer to the head of the list
- *
- * Return: the number of nodes printed
- */
+* print_dlistint - Prints all the elements of a list_t list
+* @h: pointer to the head of the list
+*
+* Return: the number of nodes printed
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	for (; h != NULL; h = h->next)
-	{
-		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-		printf("[%u] %s\n", h->len, h->str);
-		}
+		printf("%d\n", h->n);
 		count++;
-	}
+		}
 	return (count);
 }
