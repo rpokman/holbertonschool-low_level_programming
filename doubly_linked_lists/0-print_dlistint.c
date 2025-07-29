da7075465102507c581728ea9a8e116bc9dfafdd
rpokman <roymathis74200@gmail.com>
2025-07-29 09:08:58 +0200

    feat: implement print_dlistint function to print elements of a doubly linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
+		count++;
+	}
+	return (count);
+}
