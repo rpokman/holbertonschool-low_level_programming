08b6325801613ada63712829f19c8ef0f58f1fa8
rpokman <roymathis74200@gmail.com>
2025-07-31 10:35:48 +0200

    Fix read_textfile betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,50 +4,52 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints its content to the terminal.
- * @filename: The name of the file to read.
- * @letters: The maximum number of letters to read and print.
- *
- * Return: The actual number of letters read and printed,
- *         or 0 on failure (if filename is NULL, or if reading or writing fails).
- */
+* read_textfile - Reads a text file and prints its content to the terminal.
+* @filename: The name of the file to read.
+* @letters: The maximum number of letters to read and print.
+*
+* Return: The actual number of letters read and printed,
+*         or 0 on failure (if filename is NULL, or if reading or writing fails).
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buffer;
-    ssize_t read_bytes, written_bytes;
+	int fd;
 
-    if (filename == NULL)
-        return (0);
+	char *buffer;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	ssize_t read_bytes, written_bytes;
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	if (filename == NULL)
+		return (0);
 
-    read_bytes = read(fd, buffer, letters);
-    if (read_bytes == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-    if (written_bytes == -1 || written_bytes != read_bytes)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
-    return (written_bytes);
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (written_bytes == -1 || written_bytes != read_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (written_bytes);
 }
