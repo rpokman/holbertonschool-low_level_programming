31b7ee7b9c3f1b6e14edc837c7a23c24ab2bebe7
rpokman <roymathis74200@gmail.com>
2025-08-01 14:22:35 +0200

    fix 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,119 +1,73 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-void print_error(int code, const char *message, const char *arg);
-void copy_file(const char *file_from, const char *file_to);
+#define BUFFER_SIZE 1024
 
 /**
- * main - Entry point. Checks arguments and starts file copy.
- * @ac: Argument count.
- * @av: Argument vector.
- *
- * Return: 0 on success, exits with codes 97 to 100 on failure.
+ * print_error - Prints an error message to stderr and exits.
+ * @exit_code: Exit code.
+ * @msg: The message to print.
+ * @arg: Additional string (filename or fd).
  */
-int main(int ac, char **av)
+void print_error(int exit_code, const char *msg, const char *arg)
 {
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	copy_file(av[1], av[2]);
-	return (0);
+	dprintf(STDERR_FILENO, "%s %s\n", msg, arg);
+	exit(exit_code);
 }
 
 /**
- * copy_file - Copies content from one file to another.
- * @file_from: Source file.
- * @file_to: Destination file.
+ * main - Copies content of one file to another.
+ * @ac: Argument count.
+ * @av: Argument vector.
+ *
+ * Return: 0 on success, exits with 97-100 on error.
  */
-void copy_file(const char *file_from, const char *file_to)
+int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t read_bytes, written_bytes;
-	char buffer[1024];
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-	fd_from = open(file_from, O_RDONLY);
+	if (ac != 3)
+		print_error(97, "Usage:", "cp file_from file_to");
+
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file", file_from);
+		print_error(98, "Error: Can't read from file", av[1]);
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		if (close(fd_from) == -1)
-		{
-			write(STDERR_FILENO, "Error: Can't close fd ", 23);
-			dprintf(STDERR_FILENO, "%d\n", fd_from);
-			exit(100);
-		}
-		print_error(99, "Error: Can't write to", file_to);
+		close(fd_from);
+		print_error(99, "Error: Can't write to", av[2]);
 	}
 
-	while ((read_bytes = read(fd_from, buffer, 1024)) > 0)
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		written_bytes = write(fd_to, buffer, read_bytes);
-		if (written_bytes != read_bytes)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
-			if (close(fd_from) == -1)
-			{
-				write(STDERR_FILENO, "Error: Can't close fd ", 23);
-				dprintf(STDERR_FILENO, "%d\n", fd_from);
-				exit(100);
-			}
-			if (close(fd_to) == -1)
-			{
-				write(STDERR_FILENO, "Error: Can't close fd ", 23);
-				dprintf(STDERR_FILENO, "%d\n", fd_to);
-				exit(100);
-			}
-			print_error(99, "Error: Can't write to", file_to);
+			close(fd_from);
+			close(fd_to);
+			print_error(99, "Error: Can't write to", av[2]);
 		}
 	}
-
-	if (read_bytes == -1)
-	{
-		if (close(fd_from) == -1)
-		{
-			write(STDERR_FILENO, "Error: Can't close fd ", 23);
-			dprintf(STDERR_FILENO, "%d\n", fd_from);
-			exit(100);
-		}
-		if (close(fd_to) == -1)
-		{
-			write(STDERR_FILENO, "Error: Can't close fd ", 23);
-			dprintf(STDERR_FILENO, "%d\n", fd_to);
-			exit(100);
-		}
-		print_error(98, "Error: Can't read from file", file_from);
-	}
+	if (bytes_read == -1)
+		print_error(98, "Error: Can't read from file", av[1]);
 
 	if (close(fd_from) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd ", 23);
-		dprintf(STDERR_FILENO, "%d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
 	if (close(fd_to) == -1)
 	{
-		write(STDERR_FILENO, "Error: Can't close fd ", 23);
-		dprintf(STDERR_FILENO, "%d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-}
 
-/**
- * print_error - Prints an error message and exits.
- * @code: Exit code.
- * @message: Error message.
- * @arg: Argument to show.
- */
-void print_error(int code, const char *message, const char *arg)
-{
-	dprintf(STDERR_FILENO, "%s %s\n", message, arg);
-	exit(code);
+	return (0);
 }
