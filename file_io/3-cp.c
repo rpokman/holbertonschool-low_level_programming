0e01b89e206f0e8518857cf7fbc241e322a52717
rpokman <roymathis74200@gmail.com>
2025-08-03 10:12:52 +0200

    fix 3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,112 +1,67 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-void print_error(int code, const char *message, const char *arg);
-void copy_file(const char *file_from, const char *file_to);
+#define BUF_SIZE 1024
 
 /**
- * main - Entry point. Checks arguments and starts file copy.
- * @ac: Argument count.
- * @av: Argument vector.
- *
- * Return: 0 on success, exits with codes 97 to 100 on failure.
+ * print_error - Prints an error message and exits with the given code
+ * @exit_code: The exit code
+ * @message: The error message
+ * @arg: Argument to include in the message (like a filename or fd)
  */
-int main(int ac, char **av)
+void print_error(int exit_code, const char *message, const char *arg)
 {
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	copy_file(av[1], av[2]);
-	return (0);
+	dprintf(STDERR_FILENO, message, arg);
+	exit(exit_code);
 }
 
 /**
- * copy_file - Copies content from one file to another.
- * @file_from: Source file.
- * @file_to: Destination file.
+ * main - Copies the content of a file to another file
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, or exit with error code
  */
-void copy_file(const char *file_from, const char *file_to)
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t read_bytes, written_bytes;
-	char buffer[1024];
+	int fd_from, fd_to, r, w;
+	char buffer[BUF_SIZE];
 
-	fd_from = open(file_from, O_RDONLY);
+	if (argc != 3)
+		print_error(97, "Usage: %s file_from file_to\n", argv[0]);
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file", file_from);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		if (close(fd_from) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-			exit(100);
-		}
-		print_error(99, "Error: Can't write to", file_to);
+		close(fd_from);
+		print_error(99, "Error: Can't write to %s\n", argv[2]);
 	}
 
-	while ((read_bytes = read(fd_from, buffer, 1024)) > 0)
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
-		written_bytes = write(fd_to, buffer, read_bytes);
-		if (written_bytes != read_bytes)
+		w = write(fd_to, buffer, r);
+		if (w != r)
 		{
-			if (close(fd_from) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-				exit(100);
-			}
-			if (close(fd_to) == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-				exit(100);
-			}
-			print_error(99, "Error: Can't write to", file_to);
+			close(fd_from);
+			close(fd_to);
+			print_error(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
-
-	if (read_bytes == -1)
+	if (r == -1)
 	{
-		if (close(fd_from) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-			exit(100);
-		}
-		if (close(fd_to) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-			exit(100);
-		}
-		print_error(98, "Error: Can't read from file", file_from);
+		close(fd_from);
+		close(fd_to);
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
 	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		print_error(100, "Error: Can't close fd %d\n", argv[1]);
 
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-}
+		print_error(100, "Error: Can't close fd %d\n", argv[2]);
 
-/**
- * print_error - Prints an error message and exits.
- * @code: Exit code.
- * @message: Error message.
- * @arg: Argument to show.
- */
-void print_error(int code, const char *message, const char *arg)
-{
-	dprintf(STDERR_FILENO, "%s %s\n", message, arg);
-	exit(code);
+	return (0);
 }
